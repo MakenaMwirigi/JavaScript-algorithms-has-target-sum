64eba59187fa5b40e3d3ed7d8bba45cf3a1c4362
MakenaMwirigi <makenasharon46@gmail.com>
2025-05-26 17:28:28 +0300

    algorithm has target sum

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set();
+
+  for (let num of array) {
+    const complement = target - num;
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+    seenNumbers.add(num);
+  }
+
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
+  Time complexity: O(n), where n is the length of the array.
+  Space complexity: O(n) for the set storing seen numbers.
 */
 
 /*
-  Add written explanation of your solution here
+  Explanation:
+  We're trying to find if two numbers in the array add up to the target.
+  Instead of comparing every pair (which would be O(n^2)), we use a set
+  to store numbers we've seen so far and check for the complement.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +38,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  // Additional tests
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([5, 5], 10));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([], 10));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([10], 10));
 }
 
 module.exports = hasTargetSum;
